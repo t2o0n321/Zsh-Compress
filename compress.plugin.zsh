5762c673b75fc79a2f6cdee7afc0c03a0444dcb3
t2o0n321 <i915915514@icloud.com>
2022-12-15 20:30:25 +0800

    commit

---
+++ compress.plugin.zsh
@@ -0,0 +1,92 @@
+help(){
+	echo "- Usage: compress [targetFile] [outFile]"
+}
+
+getExt(){
+	local outFilePath=$1
+	local ext=$(echo $outFilePath | rev | cut -d "/" -f 1 | cut -d "." -f 1 | rev)
+	echo $ext
+}
+
+checkCommand(){
+	local toCheck=$1
+	if ! command -v $toCheck &> /dev/null
+	then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+compress(){
+	# Pending Input
+	if ! [ "$#" -ge 2 ]; then
+		echo "Syntax error"
+		help
+	else
+		local targetFolder=$1
+		local outFilePath=$2
+		local ext=$(getExt $outFilePath)
+
+		case "${outFilePath:l}" in
+			(*.rar)
+				if [ $(checkCommand rar) -eq "0" ]; then
+					echo "Installing rar ... "
+					sudo apt install rar -y
+				fi
+				rar a $outFilePath $targetFolder
+				;;
+			(*.zip)
+				if [ $(checkCommand zip) -eq "0" ]; then
+					echo "Installing zip ... "
+					sudo apt install zip -y
+				fi
+				zip -r $outFilePath $targetFolder
+				;;
+			(*.7z)
+				if [ $(checkCommand 7z) -eq "0" ]; then
+					echo "Installing 7z ... "
+					sudo apt install p7zip-full -y
+				fi
+				7z a $outFilePath $targetFolder
+				;;
+			(*.bz2)
+				if [ $(checkCommand tar) -eq "0" ]; then
+					echo "Installing tar ... "
+					sudo apt install tar -y
+				fi
+				if [ $(checkCommand bzip2) -eq "0" ]; then
+					echo "Installing tar ... "
+					sudo apt install bzip2 -y
+				fi
+				tar -cf "${outFilePath}.tar" $targetFolder && bzip2 "${outFilePath}.tar" && mv "${outFilePath}.tar.bz2" $outFilePath
+				;;
+			(*.tar)
+				if [ $(checkCommand tar) -eq "0" ]; then
+					echo "Installing tar ... "
+					sudo apt install tar -y
+				fi
+				tar cvf $outFilePath $targetFolder
+				;;
+			(*.tar.xz)
+				if [ $(checkCommand tar) -eq "0" ]; then
+					echo "Installing tar ... "
+					sudo apt install tar -y
+				fi
+				tar Jcvf $outFilePath $targetFolder
+				;;
+			(*.tar.gz | *.tgz)
+				if [ $(checkCommand tar) -eq "0" ]; then
+					echo "Installing tar ... "
+					sudo apt install tar -y
+				fi
+				tar zcvf $outFilePath $targetFolder
+				;;
+			(*)
+				echo "Fail to compress to $outFilePath ... "
+				echo "Extension '$ext' is not supported ... "
+				;;
+		esac
+
+	fi
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Zsh-Compress
+簡單的壓縮懶人包插件。
+# Installation
+```
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+```
+# Usage
+# Supported Compression Format
